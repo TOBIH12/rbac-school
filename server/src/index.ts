f06f705aad96406a8936755f201ed05426b6b3fb
TOBIH12 <ayomikunbolaji43@gmail.com>
2026-08-12 02:11:39 +0100

    feat: made adjustmensts in configuration of cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import './types';
 import express from 'express';
-import dotenv from 'dotenv';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import compression from 'compression'
 import usersV1 from './routes/v1/userRoutes';
 import postsV1 from './routes/v1/postsRoutes';
@@ -11,10 +11,17 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors({origin: 'http://localhost:5173', credentials: true})); // Allow CORS for the frontend app
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Replace with frontend URL
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.use(express.json());
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 app.use('/api/edu/users', usersV1);
 app.use('/api/edu/posts', postsV1);
