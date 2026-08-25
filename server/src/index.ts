e37f89b4bddce0c039c580de4e9062c1f9c2016d
TOBIH12 <ayomikunbolaji43@gmail.com>
2026-08-25 03:22:22 +0100

    build: frontend url

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,9 +11,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({origin: 'http://localhost:5173', credentials: true})); // Allow CORS for the frontend app
+app.use(cors({origin: 'https://rbac-school.vercel.app', credentials: true})); // Allow CORS for the frontend app
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Replace with frontend URL
+    res.header('Access-Control-Allow-Origin', 'https://rbac-school.vercel.app/announcements'); // Replace with frontend URL
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
