9176d634bb4b94364a91a1810378737758c2a08b
TOBIH12 <ayomikunbolaji43@gmail.com>
2026-08-25 01:42:57 +0100

    fix: port issues

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,5 +9,5 @@ pool
   .then(() => console.log('Connected to PostgreSQL database'))
   .catch((err) => console.error('Database connection error', err));
 
-const PORT = process.env.PORT || 8080;
-app.listen(8080, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+const PORT = Number(process.env.PORT);
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
