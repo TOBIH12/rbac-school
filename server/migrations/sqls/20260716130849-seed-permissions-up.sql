2a1976afb6e52825925bea2ac7fb39b4bd784f02
TOBIH12 <ayomikunbolaji43@gmail.com>
2026-08-25 01:29:07 +0100

    fix: migrations duplicate

--- server/migrations/sqls/20260716130849-seed-permissions-up.sql
+++ server/migrations/sqls/20260716130849-seed-permissions-up.sql
@@ -5,6 +5,4 @@ INSERT INTO public.permissions (id, name) VALUES
 (101, 'manage_courses'),
 (102, 'view_system_logs'),
 (103, 'view_students'),
-(104, 'grade_students'),
-(105, 'view_own_profile'),
 (106, 'view_grades');
