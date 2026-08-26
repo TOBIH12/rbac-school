bea19d559e9b55c70f5d9ee4db59e045d72a4df5
TOBIH12 <ayomikunbolaji43@gmail.com>
2026-08-26 12:08:38 +0100

    #

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,14 +11,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({origin: 'https://rbac-school.vercel.app', credentials: true})); // Allow CORS for the frontend app
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://rbac-school.vercel.app/announcements'); // Replace with frontend URL
+    res.header('Access-Control-Allow-Origin', 'https://rbac-school.vercel.app'); // Replace with frontend URL
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
+app.use(cors({origin: 'https://rbac-school.vercel.app', credentials: true})); // Allow CORS for the frontend app
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
